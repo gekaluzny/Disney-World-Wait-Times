16b573809c3b9db46a091f2d2e30ad0de91a77a6
gekaluzny <52294276+gekaluzny@users.noreply.github.com>
2019-08-04 14:31:07 -0400

    Update avg_waits.R

--- avg_waits.R
+++ avg_waits.R
@@ -6,30 +6,52 @@ setwd("/Users/grahamkaluzny/Documents/magic_kingdom/")
 files <- list.files()
 
 ride_names <- files %>%
-  str_replace("_.*", "") %>%
+  str_extract(".*(?=_)") %>%
+  discard(~ .x=="hours") %>%
   unique()
 
 data <- ride_names %>%
   map(~ str_subset(string = files, pattern = .x)) %>%
   set_names(ride_names) %>%
   map(map_dfr, read_csv, col_types = "cd") %>%
-  map(mutate_at, "time", as_datetime)
+  map(mutate_at, "datetime", as_datetime)
 
-avg_waits <- data %>%
-  map(mutate_at, "time", round_date, "10 minutes") %>%
-  map(group_by, time) %>%
-  map(summarize_all, mean) %>%
-  map(mutate, "date" = date(time), "time" = strftime(time, "%H:%M:%S")) %>%
+hours_files <- list.files(pattern = "hours")
+
+dates <- hours_files %>%
+  str_extract("(?<=hours_).{10}") %>%
+  ymd()
+
+hours <- hours_files %>%
+  map_dfr(read_csv, col_types = "tt") %>%
+  mutate(date = dates)
+
+dates_9_11 <- hours %>%
+  filter(opening==as.hms("09:00:00") & closing==as.hms("23:00:00")) %>%
+  pull(date)
+  
+avg_waits_9_11 <- data %>%
+  map(mutate, datetime_round = round_date(datetime, "10 minutes")) %>%
+  map(group_by, datetime_round) %>%
+  map(summarize_at, "wait", mean, na.rm = TRUE) %>%
+  map(mutate, "date" = date(datetime_round), "time" = strftime(datetime_round, "%H:%M:%S"), 
+      "wday" = wday(datetime_round)) %>%
+  map(select, -datetime_round) %>%
+  map(filter, date %in% dates_9_11) %>%
+  map(filter, wday!=6) %>%
   map(spread, key = "time", value = "wait") %>%
   map(summarize_at, vars(-date), mean, na.rm = TRUE) %>%
   map(gather, key = "time", value = "wait") %>%
   map(mutate_at, "time", as.hms) %>%
   reduce(full_join, by = "time") %>%
   setNames(c("time", ride_names)) %>%
-  filter(time >= as.hms("09:00:00") & time <= as.hms("22:00:00"))
+  filter(time >= as.hms("09:00:00") & time <= as.hms("23:00:00"))
   
+write_csv(avg_waits_9_11, "/Users/grahamkaluzny/Documents/avg_waits_9_11.csv")
   
-
+long <- avg_waits_9_11 %>%
+  gather(key = "ride", value = "wait", -time) %>%
+  write_csv("/Users/grahamkaluzny/Documents/magic_kingdom.csv")
   
 
       
